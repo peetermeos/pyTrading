dce57d700a5ca4710e1dfa44f479076c890db2b8
Peeter Meos <peeter@sigmaresearch.eu>
2017-09-12 22:07:34 +0300

    TF now is accessible

---
+++ sigma/ai/rnn.py
@@ -0,0 +1,27 @@
+'''
+Training of first naive RNN
+
+Created on Sep 12, 2017
+
+@author: Peeter Meos
+@version: 0.1
+'''
+
+import tensorflow as tf
+
+def loadData():
+    pass
+
+def train():
+    pass
+
+def main():
+    tf.abs(5, None)
+    
+    loadData()
+    train()
+
+## Main entry point to the program
+if __name__ == "__main__":
+    main()
+    
